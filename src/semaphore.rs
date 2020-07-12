b5d4ff6073b0f857484a244160ad192b6f5ce685
Carlos Teixeira <c.mh.teixeira@gmail.com>
2020-07-12 14:27:39 +0100

    Fix semaphore?

--- src/semaphore.rs
+++ src/semaphore.rs
@@ -2,32 +2,35 @@ use std::sync::{Mutex, Condvar};
 use std::ops::{DerefMut, Deref};
 
 pub struct Semaphore {
-    counter: Mutex<u8>,
+    counter: Mutex<(u8, bool)>,
     condvar: Condvar
 }
 
 impl Semaphore {
     pub fn new(u: u8) -> Semaphore {
         Semaphore {
-            counter: Mutex::new(u),
+            counter: Mutex::new((u, if u <= 0 {false} else {true})),
             condvar: Condvar::new()
         }
     }
 
     pub fn increment(&self) -> () {
-        let mut mutex_guard = self.counter.lock().expect("Can't lock");
-        *mutex_guard = *mutex_guard + 1;
-        if *mutex_guard >= 1 {
-            self.condvar.notify_all();
-        }
+        let mut mutex_guard= self.counter.lock().expect("Can't lock");
+        let (mut counter, mut bool) = mutex_guard.deref_mut();
+        bool = true;
+        counter = counter + 1;
+        self.condvar.notify_one();
     }
 
     pub fn decrement(&self) -> () {
         let mut mutex_guard = self.counter.lock().expect("Can't lock");
-        while ! (mutex_guard.ge(&1)) {  // guard against spurious awakes.
+        let (mut counter, mut bool) = mutex_guard.deref_mut();
+        while !bool {  // guard against spurious awakes.
             mutex_guard = self.condvar.wait(mutex_guard).expect("Can't wait.");
         }
-        let mut counter = mutex_guard.deref_mut();
-        *counter = *counter - 1;
+        counter = counter - 1;
+        if counter == 0 {
+            bool = false;
+        }
     }
 }
