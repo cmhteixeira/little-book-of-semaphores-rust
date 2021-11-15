803cf71ecc5cc5dad4d8548126d676c3bb9e0625
Carlos Teixeira <c.mh.teixeira@gmail.com>
2021-11-15 22:02:36 +0000

    Test changes

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,14 @@
-use little_book_semaphores_rust::producer_consumer::BlockingQueue;
-use std::thread;
 use std::sync::Arc;
+use std::sync::atomic::{AtomicPtr, Ordering};
+use std::thread;
+use std::thread::JoinHandle;
 use std::time::Duration;
-use little_book_semaphores_rust::readers_writers::ReadersWritersLock;
 use rand::Rng;
+use std::cell::RefMut;
+use std::rc::Rc;
+
 
 fn main() {
-    let mut my_initial = vec!["a".to_string(), "b".to_string(), "c".to_string()];
-    let rw_lock = ReadersWritersLock::new(&mut my_initial);
-    let rw_lock = Arc::new(rw_lock);
-
-    let mut threads = vec![];
-    for i in 1..5 {
-        let rw_lock = rw_lock.clone();
-        let thread_handle = thread::Builder::new().name(String::from(format!("WriterThread:{}/100", i)))
-            .spawn(move || unsafe {
-                // let mut current_value: Vec<String> = vec![];
-                let mut iter = 0;
-                loop {
-                    iter += 1;
-                    println!("I am writer thread {}, outside", i);
-                    thread::sleep(Duration::from_millis(100));
-                    rw_lock.write(|a| {
-                        println!("I am writer thread {}, currently on the critical section", i);
-                        let num = rand::thread_rng().gen_range(20..30);
-                        thread::sleep(Duration::from_millis(num * 10));
-                        let new_value = a.into_iter().map(|k| format!("{}-{}", k, i)).collect();
-                        new_value
-                    });
-                    if iter == 20 {break;}
-                }
-            }).expect("Can't create thread ...");
-        threads.push(thread_handle);
-    }
-
-    for i in 1..4 {
-        let rw_lock = rw_lock.clone();
-        let thread_handle = thread::Builder::new().name(String::from(format!("ReaderThread:{}/100", i)))
-            .spawn(move || {
-                let mut iter = 0;
-                loop {
-                    iter += 1;
-                    let res = rw_lock.read(|protected_resource| {
-                        let num = rand::thread_rng().gen_range(1..5);
-                        thread::sleep(Duration::from_millis(num * 1000));
-                        protected_resource.clone()
-                    });
-                    println!("I am reader thread {}, currently on the critical section. The content is:\n{} ", i, res.join(" # "));
-                    if iter == 20 {break;}
-                }
-            }).expect("Can't create thread ...");
-        threads.push(thread_handle);
-    }
-
-    for i in threads {
-        i.join();
-    }
+    // let t = std::ptr::null<usize>();
+    RefMut::map()
 }
--- src/readers_writers.rs
+++ src/readers_writers.rs
@@ -6,6 +6,7 @@ use std::fmt::{Display, Debug};
 use rand::Rng;
 
 pub struct ReadersWritersLock<T: Debug> {
+    value: T,
     elem: AtomicPtr<T>,
     readers_counter: atomic::AtomicU64,
     readers_s: Semaphore,
@@ -13,9 +14,13 @@ pub struct ReadersWritersLock<T: Debug> {
 }
 
 impl<T: Debug> ReadersWritersLock<T> {
-    pub fn new(t: &mut T) -> ReadersWritersLock<T> {
+    pub fn new(mut t: T) -> ReadersWritersLock<T> {
+        // let mut foo = t;
+        // let pointer = AtomicPtr::new(&mut t);
+
         ReadersWritersLock {
-            elem: AtomicPtr::new(t),
+            elem: AtomicPtr::new(&mut t),
+            value: t,
             readers_counter: AtomicU64::new(0),
             readers_s: Semaphore::new(1),
             writers_s: Semaphore::new(1),
@@ -37,6 +42,7 @@ impl<T: Debug> ReadersWritersLock<T> {
         // Read critical section - Begin
         let result;
         unsafe {
+            println!("Read..");
             let current_value = self.elem.load(Ordering::SeqCst);
             result = read_op(&*current_value);
         }
@@ -64,6 +70,24 @@ impl<T: Debug> ReadersWritersLock<T> {
         }
         // Writers critical section
 
+        println!("this is");
+        println!("This is the element {:?}", self.value);
         self.writers_s.increment();
+    }
+}
+
+
+impl<T: Debug> Drop for ReadersWritersLock<T> {
+   fn drop(&mut self) {
+       println!("Dropping ...");
+       // std::thread::sleep(std::time::Duration::from_millis(2000));
+       println!("Dropping2 ...");
+       println!("The readers counter is: {:?}", self.readers_counter.load(Ordering::SeqCst));
+       unsafe {
+           println!("The pointer is: {:?}", *self.elem.load(Ordering::SeqCst));
+       }
+       // println!("The value is: {:?}", self.value);
+       println!("Dropping3 ...");
+       println!("The reference is: {:?}", self.elem.load(Ordering::SeqCst));
     }
 }
