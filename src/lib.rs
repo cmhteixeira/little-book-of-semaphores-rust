c60ae01c3f748b5c9496b04e071717335035bce1
Carlos Teixeira <c.mh.teixeira@gmail.com>
2022-03-27 18:25:57 +0100

    Delete the seriously incorrect semaphore implementation

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,8 @@ pub mod queue;
 pub mod dining_savages;
 pub mod producer_consumer;
 pub mod readers_writers;
-pub mod semaphore_fixed;
-pub mod semaphore_fixed_2;
-pub mod semaphore_fixed_3;
+pub mod semaphore_2;
+pub mod semaphore_3;
 pub mod semaphore_simplest;
 
 pub use semaphore::Semaphore as Semaphore;
--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,8 @@ use std::time;
 use rand::Rng;
 
 use little_book_semaphores_rust::readers_writers::ReadersWritersLock;
-use little_book_semaphores_rust::semaphore_fixed_3::{Semaphore as SemaphoreFixed3};
-use little_book_semaphores_rust::semaphore_fixed_2::{Semaphore as SemaphoreFixed2};
-use little_book_semaphores_rust::semaphore_fixed::{Semaphore as SemaphoreFixed};
+use little_book_semaphores_rust::semaphore_3::{Semaphore as SemaphoreFixed3};
+use little_book_semaphores_rust::semaphore_2::{Semaphore as SemaphoreFixed2};
 use little_book_semaphores_rust::semaphore_simplest::{Semaphore as SemaphoreSimplest};
 use little_book_semaphores_rust::Semaphore;
 
